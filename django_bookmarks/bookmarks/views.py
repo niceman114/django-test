35f89bd9b26fd32381b58c56ee79a4c22ce2c37e
Seokgeun Oh <niceman114@gmail.com>
2017-08-05 18:08:41 +0900

    add view : main_page

--- django_bookmarks/bookmarks/views.py
+++ django_bookmarks/bookmarks/views.py
@@ -1,6 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def main_page(request):
+    ouput = '''
+        <html>
+            <head>
+                <title>%s</title>
+            </head>
+            <body>
+                <h1>%s</h1><p>%s</p>
+            </body>
+        </html>
+    ''' % ( 
+            '장고 | 북마크',
+            '장고 북마크 웰컴!',
+            '북마크 저장해 줘요~'
+    )
+
+    return HttpResponse(ouput)
